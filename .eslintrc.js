2b6a7a4b15129f0a5c826e0f71df3d8a79a47f81
Anuda Weerasinghe <anuda@live.com>
2023-01-20 16:33:49 -0500

    changes fib to ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,19 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: ["*.js"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  ignorePatterns: ["*.js", "*.test.ts"],
 
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    project: ["tsconfig.json"],
   },
   plugins: ["@typescript-eslint"],
   rules: {},
