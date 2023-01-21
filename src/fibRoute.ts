7b685fbb379cdf1b11c2463f71c93e5b178e425b
Anuda Weerasinghe <anuda@live.com>
2023-01-21 18:15:28 -0500

    separates fib function and route

---
+++ src/fibRoute.ts
@@ -0,0 +1,16 @@
+// Endpoint for querying the fibonacci numbers
+
+const fibonacci = require("./fib");
+
+module.exports = (req, res) => {
+  const { num } = req.params;
+
+  const fibN = fibonacci(parseInt(num));
+  let result = `fibonacci(${num}) is ${fibN}`;
+
+  if (fibN < 0) {
+    result = `fibonacci(${num}) is undefined`;
+  }
+
+  res.send(result);
+};
